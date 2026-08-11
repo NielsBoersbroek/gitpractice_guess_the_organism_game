17e6f91e852a5317569b23bbec6fa0c7d6031e2b
NielsBoersbroek <niels.boersbroek@gmail.com>
2026-08-11 12:23:12 +0200

    Create and start Guess the Organism Python script
    The file has been created and the first few lines of coding have been
    written, such as importing modules, loading in .csv file and examining
    dataframe. This was purposfully already documented as a git commit.

---
+++ scripts/game.py
@@ -0,0 +1,29 @@
+## About this Python script ----------------------------------------------------
+
+# Title: Guess the Organism Game
+# Author: Niels Boersbroek
+# Date: 11-08-2026
+# Conda environment: guess_the_organism
+
+
+## Import modules --------------------------------------------------------------
+
+
+import pandas as pd
+import numpy as np
+
+
+## Opening data from disk ------------------------------------------------------
+
+
+# My Excel is in a different language than English and thus uses
+# ; as a seperator when creating csv files
+organisms = pd.read_csv("data/organism_data.csv", sep = ";")
+# Examine the first five rows
+organisms_head = organisms.head(5)
+print("The top 5 lines of the organisms DataFrame:\n", organisms_head)
+# Examine the number of rows and columns
+organisms.shape # shape is the equivalent of R's dim()
+# Get column names
+organisms.columns
+
